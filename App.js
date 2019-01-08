090e20f8fb44c44b0bba22157ec48c13565c4978
shin419 <shin419@users.noreply.github.com>
2019-01-08 09:11:39 +0700

    styles
    styles

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import styles from './src/styles';
 export default class App extends React.Component {
   render() {
     return (
@@ -11,11 +11,3 @@ export default class App extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inpText: {width:200, height:40, backgroundColor:'#448AFF',  color:'#fff', marginBottom:20 , paddingHorizontal:10},
+  btnLogin:{width:200, borderRadius:20, paddingVertical:10, backgroundColor:'#448AFF'},
+  touchbtn:{textAlign:'center'},
+  itemflat:{flex:1, height:113, borderColor:'#000', borderWidth:2,marginTop:2},
+
+})
+module.exports = styles;
